319994615434f47ef91b829bd209e3b791b27241
qu4resm4 <quaresmacont@gmail.com>
2025-10-22 18:52:31 -0300

    feat(ft_memset): aprendi ft_memset com brits e gaya

---
+++ ft_memset.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gquaresm <gquaresm@student.42.rio>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/22 12:11:08 by gquaresm          #+#    #+#             */
+/*   Updated: 2025/10/22 12:58:33 by gquaresm         ###   ########.rio      */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdlib.h>
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	unsigned char	const_byte;	
+	unsigned int	index;
+	char			*cast_ptr;
+
+	cast_ptr = (char *) s;
+	const_byte = c;
+	index = 0;
+	while (index != n)
+	{
+		cast_ptr[index] = const_byte;
+		index++;
+	}
+	return (s);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+
+int main () 
+{
+   char str[50];
+
+   strcpy(str, "Welcome to Tutorialspoint");
+   puts(str);
+
+   memset(str, '#', 7);
+   puts(str);
+   
+   return(0);
+}
+*/
