996a6eb43831b995bc02304dc262077a4edf44a6
qu4resm4 <quaresmacont@gmail.com>
2025-11-16 17:47:40 -0300

    feat(split): funcionando, falta por na norma

--- ft_split.c
+++ ft_split.c
@@ -6,19 +6,10 @@
 /*   By: gquaresm <gquaresm@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/15 22:43:37 by gquaresm          #+#    #+#             */
-/*   Updated: 2025/11/16 10:59:03 by gquaresm         ###   ########.rio      */
+/*   Updated: 2025/11/16 17:46:48 by gquaresm         ###   ########.rio      */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
-
-static char	*ft_strtrim_chr(char const *s1, char c)
-{
-	char	str[2];
-
-	str[0] = c;
-	str[1] = '\0';
-	return (ft_strtrim(s1, c));
-}
 
 static void	ft_count_words(char const *s, char c, unsigned int *count)
 {
@@ -39,39 +30,67 @@ static void	ft_count_words(char const *s, char c, unsigned int *count)
 	}
 }
 
+static char	*ft_alloc_and_cpy_str(char const *s, char c, unsigned int index)
+{
+	char	*str;
+	unsigned int	count_letters;
+	unsigned int	index_word;
+	short	last_is_c;
+
+	last_is_c = 1;
+	count_letters = 0;
+	index_word = 0;
+	while (*s && index_word != index)
+	{
+		if (last_is_c && (*s != c))
+		{
+			index_word++;
+			last_is_c = 0;
+			while (index_word == index
+				&& s[count_letters] != c && s[count_letters])
+				count_letters++;
+		}
+		if (*s == c)
+			last_is_c = 1;
+		s++;
+	}
+	str = (char *) malloc (count_letters + 1);
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, s - 1, count_letters + 1);
+	return (str);
+}
+// 24 linhas
+
+static void	ft_free_str_arr(char	**str_arr)
+{
+	while (*str_arr)
+	{
+		free(*str_arr);
+		(*str_arr)++;
+	}
+	free(str_arr);
+}
+
 char	**ft_split(char const *s, char c)
-{	
-	char	**str_arr;
+{
+	char			**str_arr;
 	unsigned int	count;
 
-	s = ft_strtrim_chr(s, c);
 	ft_count_words(s, c, &count);
 	str_arr = (char **) malloc(sizeof(char *) * (count + 1));
-	if (str_arr)
+	if (!str_arr)
 		return (NULL);
 	str_arr[count] = NULL;
-	// alocar espaço para as palavras
-	// preencher as palavras
-	while ()
+	while (count)
 	{
-		// iterar sobre count (qunatidade de palavras
-		// chamar função de alocar palavra (index palavra), se falhar retorna null
-		// se retorno null chama função de free iterativa (array com null no fina)
-	}		
-	free(s); // antes de retornar
+		str_arr[count - 1] = ft_alloc_and_cpy_str(s, c, count);
+		if (!str_arr[count - 1])
+		{
+			ft_free_str_arr(str_arr);
+			return (NULL);
+		}
+		count--;
+	}
 	return (str_arr);
-}
-// contar a quantidade de palavras para alocar ponteiros
-
-// iterar para contar letras palavras e allocar palaavras
-
-// preencher ponteiros e palavras e etc
-
-int	main(void)
-{
-	char	*str = "--1-2--3---4----5-----42";
-	char	**str_arr;
-
-	str_arr = ft_split(str, '-');
-	printf("%s", str_arr[0]);
 }
